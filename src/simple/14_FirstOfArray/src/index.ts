d85dce01b48975b40e8d370443d8a67435fd72ec
jiangsongyang <2744856707@qq.com>
2022-04-10 23:35:40 +0800

    [feat] : add first of array

---
+++ src/simple/14_FirstOfArray/src/index.ts
@@ -0,0 +1,25 @@
+/*
+  14 - First of Array
+  -------
+  by Anthony Fu (@antfu) #easy #array
+  
+  ### Question
+  
+  Implement a generic `First<T>` that takes an Array `T` and returns it's first element's type.
+  
+  For example
+  
+  ```ts
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [3, 2, 1]
+  
+  type head1 = First<arr1> // expected to be 'a'
+  type head2 = First<arr2> // expected to be 3
+  ```
+  
+  > View on GitHub: https://tsch.js.org/14
+*/
+
+/* _____________ 你的代码 _____________ */
+
+type First<T extends any[]> = T extends [infer First, ...any] ? First : never;
