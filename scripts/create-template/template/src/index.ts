7c4ea0e39af724fdc1ea94c07d4b09301713408c
jiangsongyang <2744856707@qq.com>
2022-04-14 17:59:27 +0800

    [feat] : add includes

--- scripts/create-template/template/src/index.ts
+++ scripts/create-template/template/src/index.ts
@@ -1,29 +1,5 @@
 /*
-  13 - Hello World
-  -------
-  by Anthony Fu (@antfu) #warm-up 
-  
-  ### Question
-  
-  Hello, World!
-  
-  In Type Challenges, we use the type system itself to do the assertion. 
-  
-  For this challenge, you will need to change the following code to make the tests pass (no type check errors).
-  
-  ```ts
-  // expected to be string
-  type HelloWorld = any
-  ```
-  
-  ```ts
-  // you should make this work
-  type test = Expect<Equal<HelloWorld, string>>
-  ```
-  
-  Click the `Take the Challenge` button to start coding! Happy Hacking!
-  
-  > View on GitHub: https://tsch.js.org/13
+ 
 */
 
 
---
+++ src/simple/898_Includes/src/index.ts
@@ -0,0 +1,34 @@
+/*
+  898 - Includes
+  -------
+  by null (@kynefuk) #easy #array
+  
+  ### Question
+  
+  Implement the JavaScript `Array.includes` function in the type system. A type takes the two arguments. The output should be a boolean `true` or `false`.
+  
+  For example
+  
+  ```ts
+  type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+  ```
+  
+  > View on GitHub: https://tsch.js.org/898
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+
+export type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? MyEqual<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
