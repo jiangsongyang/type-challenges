7ea9bc1f453f8a51ac69d9df63012fed2eb65bbc
jiangsongyang <2744856707@qq.com>
2022-04-14 17:13:20 +0800

    [feat] : add Awaited

---
+++ src/simple/189_Awaited/src/index.ts
@@ -0,0 +1,17 @@
+/*
+ 189 - Awaited
+  -------
+  by Maciej Sikora (@maciejsikora) #easy #promise #built-in
+  
+  ### Question
+  
+  If we have a type which is wrapped type like Promise. How we can get a type which is inside the wrapped type? For example if we have `Promise<ExampleType>` how to get ExampleType?
+  
+  > This question is ported from the [original article](https://dev.to/macsikora/advanced-typescript-exercises-question-1-45k4) by [@maciejsikora](https://github.com/maciejsikora)
+  
+  > View on GitHub: https://tsch.js.org/189
+*/
+
+/* _____________ Your Code Here _____________ */
+
+export type MyAwaited<T> = T extends Promise<infer R> ? MyAwaited<R> : T
---
+++ src/simple/189_Awaited/test/test.ts
@@ -0,0 +1,14 @@
+/* _____________ Test Cases _____________ */
+import { MyAwaited } from '../src'
+import { Equal, Expect } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+]
+
