5456115f6174b02359b6e29942d451f6d8b85849
jiangsongyang <2744856707@qq.com>
2022-04-14 17:15:33 +0800

    [feat] : add If

---
+++ src/simple/268_If/test/test.ts
@@ -0,0 +1,8 @@
+/* _____________ Test Cases _____________ */
+import { If } from '../src'
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+]
