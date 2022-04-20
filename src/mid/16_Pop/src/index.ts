221cc39dbc97bda171665dca336bf41b6b2e14f8
jiangsongyang <2744856707@qq.com>
2022-04-20 21:28:18 +0800

    [feat] : add Pop

---
+++ src/mid/16_Pop/src/index.ts
@@ -0,0 +1,31 @@
+/*
+   16 - Pop
+  -------
+  by Anthony Fu (@antfu) #medium #array
+  
+  ### Question
+  
+  > TypeScript 4.0 is recommended in this challenge
+  
+  Implement a generic `Pop<T>` that takes an Array `T` and returns an Array without it's last element.
+  
+  For example
+  
+  ```ts
+  type arr1 = ['a', 'b', 'c', 'd']
+  type arr2 = [3, 2, 1]
+  
+  type re1 = Pop<arr1> // expected to be ['a', 'b', 'c']
+  type re2 = Pop<arr2> // expected to be [3, 2]
+  ```
+  
+  **Extra**: Similarly, can you implement `Shift`, `Push` and `Unshift` as well?
+  
+  > View on GitHub: https://tsch.js.org/16
+*/
+
+/* _____________ Your Code Here _____________ */
+
+export type Pop<T extends any[]> = T extends [...infer R, infer Last]
+  ? R
+  : never;
