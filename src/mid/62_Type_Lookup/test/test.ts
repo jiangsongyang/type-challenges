e7845c72c83c61d81576e0b1066441a949b09d2c
jiangsongyang <2744856707@qq.com>
2022-04-22 14:53:04 +0800

    [feat] : add LookUp

---
+++ src/mid/62_Type_Lookup/test/test.ts
@@ -0,0 +1,23 @@
+/* _____________ Test Cases _____________ */
+import { LookUp } from '../src'
+import type { Equal, Expect } from '@type-challenges/utils'
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type Animal = Cat | Dog
+
+type a = LookUp<Animal, 'dog'>
+
+type cases = [
+  Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
+  Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
+]
