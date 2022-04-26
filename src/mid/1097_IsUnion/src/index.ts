6ce20a37d2b2f2accb2ab2fa8866584d8ad58458
jiangsongyang <2744856707@qq.com>
2022-04-26 21:10:57 +0800

    [feat] : add IsUnion

---
+++ src/mid/1097_IsUnion/src/index.ts
@@ -0,0 +1,27 @@
+/*
+  1097 - IsUnion
+  -------
+  by null (@bencor) #medium 
+  
+  ### Question
+  
+  Implement a type `IsUnion`, which takes an input type `T` and returns whether `T` resolves to a union type.
+  
+  For example:
+    
+    ```ts
+    type case1 = IsUnion<string>  // false
+    type case2 = IsUnion<string|number>  // true
+    type case3 = IsUnion<[string|number]>  // false
+    ```
+  
+  > View on GitHub: https://tsch.js.org/1097
+*/
+
+/* _____________ Your Code Here _____________ */
+
+export type IsUnion<T, U = T> = T extends U
+  ? [U] extends [T]
+    ? false
+    : true
+  : false;
