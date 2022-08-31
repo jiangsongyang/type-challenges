5eea5a3ccc3ca8869fd13b67f8a02ccf1c11cfc6
jiangsongyang <2744856707@qq.com>
2022-08-31 17:38:26 +0800

    [feat] : add 270 Get

---
+++ src/hard/270_Get/test/test.ts
@@ -0,0 +1,23 @@
+/* _____________ Test Cases _____________ */
+import { Get } from '../src'
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Get<Data, 'hello'>, 'world'>>,
+  Expect<Equal<Get<Data, 'foo.bar.count'>, 6>>,
+  Expect<Equal<Get<Data, 'foo.bar'>, { value: 'foobar'; count: 6 }>>,
+
+  Expect<Equal<Get<Data, 'no.existed'>, never>>,
+]
+
+type Data = {
+  foo: {
+    bar: {
+      value: 'foobar'
+      count: 6
+    }
+    included: true
+  }
+  hello: 'world'
+}
+type a = Data['hello']
