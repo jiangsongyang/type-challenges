251a17320c736a96d1fd9f4efd539ba44d14b4c2
jiangsongyang <2744856707@qq.com>
2022-09-13 09:05:20 +0800

    [feat] : add 553 DeepObjectToUniq

--- src/hard/553_DeepObjectToUnique/test/test.ts
+++ src/hard/553_DeepObjectToUnique/test/test.ts
@@ -1,5 +1,15 @@
 /* _____________ Test Cases _____________ */
 import { HelloWorld } from '../src'
-import {  } from '@type-challenges/utils'
+import type { Equal, IsFalse, IsTrue } from '@type-challenges/utils'
 
-type cases = []
+type Quz = { quz: 4 }
+
+type Foo = { foo: 2; baz: Quz; bar: Quz }
+type Bar = { foo: 2; baz: Quz; bar: Quz & { quzz?: 0 } }
+
+type UniqQuz = DeepObjectToUniq<Quz>
+type UniqFoo = DeepObjectToUniq<Foo>
+type UniqBar = DeepObjectToUniq<Bar>
+
+declare let foo: Foo
+declare let uniqFoo: UniqFoo
