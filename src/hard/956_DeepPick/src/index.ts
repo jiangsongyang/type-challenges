361b907b265408b0aa9247003c7dfdf2017d4248
jiangsongyang <2744856707@qq.com>
2022-09-25 09:05:20 +0800

    [feat] : add 956 DeepPick

--- src/hard/956_DeepPick/src/index.ts
+++ src/hard/956_DeepPick/src/index.ts
@@ -1,11 +1,44 @@
 /*
- 
+  956 - DeepPick
+  -------
+  by hiroya iizuka (@hiroyaiizuka) #hard #deep
+  
+  ### Question
+  
+  Implement a type DeepPick, that extends Utility types `Pick`.
+  A type takes two arguments.
+  
+  
+  For example:
+  
+  ```
+  
+  type obj = {
+    name: 'hoge', 
+    age: 20,
+    friend: {
+      name: 'fuga',
+      age: 30,
+      family: {
+        name: 'baz',  
+        age: 1 
+      }
+    }
+  }
+  
+  type T1 = DeepPick<obj, 'name'>   // { name : 'hoge' }
+  type T2 = DeepPick<obj, 'name' | 'friend.name'>  // { name : 'hoge' } & { friend: { name: 'fuga' }}
+  type T3 = DeepPick<obj, 'name' | 'friend.name' |  'friend.family.name'>  // { name : 'hoge' } &  { friend: { name: 'fuga' }} & { friend: { family: { name: 'baz' }}}
+  
+  ```
+  
+  > View on GitHub: https://tsch.js.org/956
 */
 
 
 /* _____________ Your Code Here _____________ */
 
-export type HelloWorld = any
+export type DeepPick = any
 
 
 
