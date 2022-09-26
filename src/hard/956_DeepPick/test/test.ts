82fbb0d0a5053b915e7a5f3e71bcddfbb59a69cb
jiangsongyang <2744856707@qq.com>
2022-09-26 09:05:20 +0800

    [feat] : add 956 DeepPick

--- src/hard/956_DeepPick/test/test.ts
+++ src/hard/956_DeepPick/test/test.ts
@@ -1,5 +1,33 @@
 /* _____________ Test Cases _____________ */
-import { HelloWorld } from '../src'
-import {  } from '@type-challenges/utils'
+import { DeepPick } from '../src'
+import type { Equal, Expect } from '@type-challenges/utils'
 
-type cases = []
+type Obj = {
+  a: number
+  b: string
+  c: boolean
+  obj: {
+    d: number
+    e: string
+    f: boolean
+    obj2: {
+      g: number
+      h: string
+      i: boolean
+    }
+  }
+  obj3: {
+    j: number
+    k: string
+    l: boolean
+  }
+}
+
+type cases = [
+  Expect<Equal<DeepPick<Obj, ''>, unknown>>,
+  Expect<Equal<DeepPick<Obj, 'a'>, { a: number }>>,
+  Expect<Equal<DeepPick<Obj, 'a' | ''>, { a: number } & unknown>>,
+  Expect<Equal<DeepPick<Obj, 'a' | 'obj.e'>, { a: number } & { obj: { e: string } }>>,
+  Expect<Equal<DeepPick<Obj, 'a' | 'obj.e' | 'obj.obj2.i'>, { a: number } & { obj: { e: string } } & { obj: { obj2: { i: boolean } } }>>,
+]
+
